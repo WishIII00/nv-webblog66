2a18ddbd7ee9c5a96ec9a9db71fb119773784a2a
WishIII00 <sarawut.nankun.01@gmail.com>
2023-08-16 12:00:47 +0700

    sarawut

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,43 @@
-let express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to Sarawut')
-})
+const app = express();
 
-app.get('/hello/:name', function(req,res){
-    console.log('hello - ' + req.params.name)
-    res.send('say hello with ' + req.params.name)
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let port = 8081
-app.listen(port, function(){
-    console.log('server runing on ' + port)
-})
+require('./route')(app)
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server belonging to nitiphumi');
+});
+
+app.get('/hello/:name', function (req, res) {
+    console.log('hello - ' + req.params.name);
+    res.send('Say hello with ' + req.params.name);
+});
+
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
+});
+
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด');
+});
+
+app.post('/user/:userId', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body));
+});
+
+app.put('/user/:userId', function (req, res) {
+    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+});
+
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body));
+});
+
+const port = 8081;
+app.listen(port, function () {
+    console.log('Server running on port ' + port);
+});
